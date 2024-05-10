5ee98609dca7e92044436eac8c56ad4daa59e2bf
Mihisandi <136303045+Mihisandi@users.noreply.github.com>
2024-05-10 17:31:12 +0530

    Update Splash.java

--- src/roy/trial/opac/Splash.java
+++ src/roy/trial/opac/Splash.java
@@ -9,18 +9,17 @@ public class Splash extends Activity {
 
     private final int SPLASH_DISPLAY_LENGHT = 5000;
 
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
 
-        /* New Handler to start the Menu-Activity 
-         * and close this Splash-Screen after some seconds.*/
+       
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+               
                 Intent mainIntent = new Intent(Splash.this,MainActivity.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
